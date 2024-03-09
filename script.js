30266508d6d712ad664e74b76e187e5e507d61b8
Wahaj <154616360+Wajii-Devp@users.noreply.github.com>
2024-03-09 19:54:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let firstName = prompt("Enter your first name");
+let lastName = prompt("Enter your last name");
+let middleName = prompt("Enter your middle name");
+
+console.log("First Name: " + firstName);
+console.log("Last Name: " + lastName);
+console.log("Middle Name: " + middleName);
